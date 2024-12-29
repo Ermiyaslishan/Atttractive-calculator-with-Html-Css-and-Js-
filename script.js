2e84d768121ed305f93be72e03d7d12496a4e6dd
Ermiyas Lishan <ermiyasyamlak3030@gmail.com>
2024-12-29 02:57:51 -0800

    committed

---
+++ script.js
@@ -0,0 +1,10 @@
+const calculate =()=>{
+    let screen = document.querySelector(".screen")
+    let buttons = document.querySelectorAll(".btn")
+    let clear = document.querySelector(".clear")
+    let equal = document.querySelector(".equal")
+
+    buttons.forEach((button)=>{
+     button.addEventListener("click",)
+    })
+    };
