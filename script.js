c4a390ec2a792e14b9408895d456a4ef61ce4945
Ermiyas Lishan <ermiyasyamlak3030@gmail.com>
2024-12-30 07:16:10 -0800

    Initial commit

--- script.js
+++ script.js
@@ -5,6 +5,18 @@ const calculate =()=>{
     let equal = document.querySelector(".equal")
 
     buttons.forEach((button)=>{
-     button.addEventListener("click",)
-    })
+     button.addEventListener("click",(e)=>{
+        let value = e.target.dataset.num;
+        screen.value += value;
+     });
+    });
+    equal.addEventListener("click",()=>{
+        let answer = eval(screen.value);
+        screen.value = answer;
+    });
+    clear.addEventListener("click",()=>{
+        screen.value="";
+    });
     };
+
+    calculate();
